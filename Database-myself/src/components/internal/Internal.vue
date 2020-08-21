6aa3ef83c517b2c349ee2cd138d6c61e5a3be5dd
chenyx <774503029@qq.com>
2020-08-21 12:06:01 +0800

    internal先出来

---
+++ Database-myself/src/components/internal/Internal.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      
+      <home></home>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import Home from './components/Home'
+export default {
+    name:'Internal',
+    components:{
+        Home
+    },
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style>
+
+</style>
--- Database-myself/src/router/index.js
+++ Database-myself/src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Internal from '@/components/internal/Internal.vue'
 Vue.use(Router)
 
-export default new Router({
+const router= new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Internal',
+      component: Internal
     }
   ]
 })
+
+export default router;
