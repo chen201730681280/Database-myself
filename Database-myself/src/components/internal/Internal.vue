5ed68e415f7c046263389c97d9adb719324a6735
chenyx <774503029@qq.com>
2020-08-21 13:21:37 +0800

    1

--- Database-myself/src/components/internal/Internal.vue
+++ Database-myself/src/components/internal/Internal.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-      
+      <all-menu></all-menu>
       <home></home>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import Home from './components/Home'
+import AllMenu from './components/AllMenu'
 export default {
     name:'Internal',
     components:{
+        AllMenu,
         Home
     },
     data(){
--- Database-myself/src/components/internal/components/Home.vue
+++ Database-myself/src/components/internal/components/Home.vue
@@ -1,7 +1,9 @@
 <template>
 
 
-
+<div>
+    asd
+</div>
 </template>
 
 <script>
